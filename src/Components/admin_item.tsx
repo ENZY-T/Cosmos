7a462bdaebf86264a10cfd133274fb49bb7e7375
Umeshan Weerasekara <76834781+UmeshanUC@users.noreply.github.com>
2021-08-29 10:47:20 +0530

    working

---
+++ src/Components/admin_item.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Classes from '../Styles/Admin_Content.module.css'
+
+const AdminContent = () => {
+    return (
+        <div className={Classes.adminContent}>
+            <h3>Admin Content Goes Here...</h3>
+        </div>
+    );
+};
+
+export default AdminContent;

d176f7ea19ca77e76f8c14d6c7db706bde5701c7
Umeshan Weerasekara <UmeshanUC@gmail.com>
2024-07-14 16:02:49 +0530

    Team Members (Team_Space) hiddent from Home Page

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,82 +1,82 @@
-import React, {useContext, useEffect} from 'react';
-import {useDispatch} from 'react-redux';
-import ArticleSpace from '../Components/article_space';
-import ProjSpace from '../Components/proj_space';
-import Classes from '../Styles/Home.module.scss';
-import QuickAbout from '../Components/QuickAbout';
-import TeamSpace from '../Components/Team_Space';
-import Mission from '../Components/Mission';
-import {StaticHostContext} from '../Context/StaticHostContext';
-import axios from "axios";
-import {serverUrl} from "../GlobalData/Global";
-import {setIsLogged, setUser} from "../Store/Slices/UserSlice";
+import React, { useContext, useEffect } from 'react'
+import { useDispatch } from 'react-redux'
+import ArticleSpace from '../Components/article_space'
+import ProjSpace from '../Components/proj_space'
+import Classes from '../Styles/Home.module.scss'
+import QuickAbout from '../Components/QuickAbout'
+import TeamSpace from '../Components/Team_Space'
+import Mission from '../Components/Mission'
+import { StaticHostContext } from '../Context/StaticHostContext'
+import axios from 'axios'
+import { serverUrl } from '../GlobalData/Global'
+import { setIsLogged, setUser } from '../Store/Slices/UserSlice'
 
 const Home = () => {
-    const dispatch = useDispatch()
+	const dispatch = useDispatch()
 
-    // dispatch(setNavState(true))
+	// dispatch(setNavState(true))
 
+	useEffect(() => {
+		// Fetching the logged user back
+		axios
+			.get(serverUrl + '/api/auth/user', { withCredentials: true })
+			.then((res) => {
+				res.status === 200 && dispatch(setIsLogged(true))
+				dispatch(setUser(res.data))
+			})
+			.catch((err) => {
+				dispatch(setIsLogged(false))
+				dispatch(setUser(null))
+			})
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, [])
 
-    useEffect(() => {
-        // Fetching the logged user back
-        axios
-            .get(serverUrl + '/api/auth/user', {withCredentials: true})
-            .then((res) => {
-                res.status === 200 &&
-                dispatch(setIsLogged(true))
-                dispatch(setUser(res.data))
-            }).catch(err => {
-            dispatch(setIsLogged(false))
-            dispatch(setUser(null))
-        })
-        // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, [])
+	//region Use Context
+	const { setActivePage } = { ...useContext(StaticHostContext) }
+	useEffect(() => {
+		setActivePage?.('home')
+	}, [])
+	//endregion
 
+	return (
+		<div className={Classes.HomeWrapper}>
+			{/*Home Cover*/}
+			<div className={Classes.cover}>
+				{/*Cover Background*/}
+				<div className={Classes.background}>
+					<div className={Classes.overlay} />
+				</div>
+				<div className={Classes.mainTitle}>
+					<p>COSMOS</p>
+					<p>Automation Systems (Pvt) Ltd.</p>
+				</div>
+				<div className={Classes.subTitle}>
+					<span>
+						Best Industrial Automation Solutions.
+						<br />
+						We Consult and Solve Your Automation Needs.
+						<br />
+					</span>
+				</div>
+				<div className={Classes.overlay} />
+			</div>
+			<div style={{ padding: '5.45% 9.75% 5.45% 9.75% ' }}>
+				<QuickAbout />
+			</div>
+			<div id='our_services'>
+				<ArticleSpace />
+			</div>
+			{/*<div className={Classes.textContent}>*/}
+			{/*    <p></p>*/}
+			{/*</div>*/}
+			<ProjSpace />
+			<Mission />
+			{/*<div className={Classes.textContent}>*/}
+			{/*    <p></p>*/}
+			{/*</div>*/}
+			{/* <TeamSpace/> */}
+		</div>
+	)
+}
 
-    //region Use Context
-    const {setActivePage} = {...useContext(StaticHostContext)};
-    useEffect(() => {
-        setActivePage?.('home')
-    }, [])
-    //endregion
-
-    return (<div className={Classes.HomeWrapper}>
-        {/*Home Cover*/}
-        <div className={Classes.cover}>
-            {/*Cover Background*/}
-            <div className={Classes.background}>
-                <div className={Classes.overlay}/>
-            </div>
-            <div className={Classes.mainTitle}>
-                <p>COSMOS</p>
-                <p>Automation Systems (Pvt) Ltd.</p>
-            </div>
-            <div className={Classes.subTitle}>
-          <span>
-            Best Industrial Automation Solutions.
-            <br/>
-            We Consult and Solve Your Automation Needs.
-            <br/>
-          </span>
-            </div>
-            <div className={Classes.overlay}/>
-        </div>
-        <div style={{padding: '5.45% 9.75% 5.45% 9.75% '}}>
-            <QuickAbout/>
-        </div>
-        <div id='our_services'>
-            <ArticleSpace/>
-        </div>
-        {/*<div className={Classes.textContent}>*/}
-        {/*    <p></p>*/}
-        {/*</div>*/}
-        <ProjSpace/>
-        <Mission/>
-        {/*<div className={Classes.textContent}>*/}
-        {/*    <p></p>*/}
-        {/*</div>*/}
-        <TeamSpace/>
-    </div>);
-};
-
-export default Home;
+export default Home
